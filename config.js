1f0fd22cc7bf8e74b9698309834a048043940a61
My-text-Git <webmy5404@gmail.com>
2024-12-08 03:33:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "qbgBmLib#OSFf9T5ezP3LyOfzZZev7M2UnAFKOUvtciK6Cg0qUek",
+SESSION_ID: process.env.SESSION_ID || "jN01XK4Q#nZKIxjAa9g4WnSaWwprzB3KUdWxxA4678wxoGxAK8bY",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/zHLW3WL/044e155205d4f11c.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "┏━━━━━━━━━━━➤◍◉➤\n│ *🔰𝐇𝐞𝐲 𝐈'𝐦 𝐀𝐯𝐢𝐬𝐡𝐤𝐚_𝐗-𝐌𝐃 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n┗━━━━━━━━━━━━━━━━━━━━\n━➤◍◉➤\n┏━━━━━━━━━━━➤◍◉➤\n│ *🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰❤️*\n┗━━━━━━━━━━\n━➤◍◉➤\n┏━━━━━━━━━━━➤◍◉➤\n│ *⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : 𝐀𝐯𝐢𝐬𝐡𝐤𝐚_𝐗 𝐓𝐞𝐜𝐡𝐧𝐨𝐥𝐨𝐠𝐲|*\n┗━━━━━━━━━━━━━━━━━━━━\n*2024🤍*\n\n┏━━━━━━━━━━━➤◍◉➤\n*This bot is made by Avishka_X technology.This is made with using Ai.You can use this bot easily.New updates will coming*\n┗━━━━━━━━━━━━━━━━━━━━\n\n🔰❤️ *join with us.....* 🔰❤️\nhttps://chat.whatsapp.com/KjDrxhc9b3f7MwmaoraCPC",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
@@ -15,7 +15,7 @@ OWNER_REACT: process.env.OWNER_REACT || "🔆",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 READ_CMD: process.env.READ_CMD || "true",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
 ALWAYS_RECORDING: process.env.ALWAYS_RECORDING || "true",
 BOT_NAME: process.env.BOT_NAME || "➺Avishka_X-MD࿐",
 AUTO_REACT: process.env.AUTO_REACT || "true",
